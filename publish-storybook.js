2b557723d04395cc4812b2173f9f411b2f45679d
Lawrence Jones <lawrjone@gmail.com>
2021-07-26 11:03:26 +0100

    Publish Storybook from CircleCI

--- publish-storybook.js
+++ publish-storybook.js
@@ -4,10 +4,12 @@ const { default: run } = require("@jamesives/github-pages-deploy-action");
 // Publishes dist/storybook into the gh-pages branch of the repo. This causes Storybook to
 // be served at our GitHub Pages site..
 run({
-  token: process.env["LAWRENCE_GITHUB_TOKEN"],
+  sshKey: true, // use the deploy key in CircleCI
   branch: "gh-pages",
   folder: "dist/storybook",
   hostname: "github.com",
   repositoryName: "gocardless/react-dropin",
   workspace: ".",
+  email: "robot@gocardless.com",
+  name: "gocardless-robot",
 });
