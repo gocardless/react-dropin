1be2bf19bffa85944a1489501ca3fb6cb0d2da97
Alexander Fenton <afenton90@gmail.com>
2022-08-04 11:56:21 +0100

    feat(react): support for react 18

--- src/GoCardlessDropinButton.tsx
+++ src/GoCardlessDropinButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import { useGoCardlessDropin } from "./hooks";
 import { GoCardlessDropinOptions } from "./types";
 import "./GoCardlessDropinButton.css";
@@ -6,13 +6,13 @@ import "./GoCardlessDropinButton.css";
 // Opens a Billing Request Flow in a modal. Relies on GoCardlessDropin being
 // loaded into window, which will only happen if the initialise script has been
 // loaded via a script tag.
-export const GoCardlessDropinButton = (options: GoCardlessDropinOptions) => {
-  const { open } = useGoCardlessDropin({ ...options });
+export const GoCardlessDropinButton: FC<GoCardlessDropinOptions> = (props) => {
+  const { open } = useGoCardlessDropin({ ...props });
 
   return (
     <button type="button" onClick={() => open()}>
-      Start Dropin for <code>{options.billingRequestFlowID}</code> in{" "}
-      <code>{options.environment}</code>
+      Start Dropin for <code>{props.billingRequestFlowID}</code> in{" "}
+      <code>{props.environment}</code>
     </button>
   );
 };
